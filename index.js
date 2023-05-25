ee0a748a9519abe56171f8a03ebe91ab62374c72
Danny Eisenga <d.eisenga@gmail.com>
2023-05-25 23:24:58 +0200

    Visual improvements, add paths to sidebar

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 // =============
 
 const $ = (...args) => document.querySelector(...args);
+
+const svgElements = ['svg', 'rect', 'path', 'g', 'image', 'defs', 'clipPath'];
 const el = (tagName, attrs = {}, children = []) => {
-  const result = tagName === 'rect'
+  const isSvg = svgElements.includes(tagName);
+  const result = isSvg
     ? document.createElementNS(SVG_NS, tagName)
     : document.createElement(tagName);
   for (const [key, value] of Object.entries(attrs)) {
@@ -25,6 +28,31 @@ const el = (tagName, attrs = {}, children = []) => {
   return result;
 };
 
+const getPathsBounds = _paths => {
+  const result = {
+    left: Infinity,
+    right: -Infinity,
+    top: Infinity,
+    bottom: -Infinity,
+  };
+  for (const path of _paths) {
+    for (const point of path.points) {
+      result.left = Math.min(result.left, point.x);
+      result.right = Math.max(result.right, point.x);
+      result.top = Math.min(result.top, point.y);
+      result.bottom = Math.max(result.bottom, point.y);
+    }
+  }
+  return {
+    left: Math.floor(result.left) - 1,
+    right: Math.ceil(result.right) + 1,
+    top: Math.floor(result.top) - 1,
+    bottom: Math.ceil(result.bottom) + 1,
+    width: result.right - result.left + 2,
+    height: result.bottom - result.top + 2,
+  };
+}
+
 // #endregion
 
 // #region DOM elements
@@ -32,13 +60,11 @@ const el = (tagName, attrs = {}, children = []) => {
 
 const loadFileEl = $('#load-file');
 const calculateButtonEl = $('#calculate');
-const pathColorEl = $('#path-color');
 const fileInputEl = $('input');
-const colorInputEl = $('#color-input');
 const canvasEl = $('canvas');
-const svgEl = $('svg');
-const pathEl = $('path');
+const svgEl = $('#editor-paths');
 const resultsEl = $('#results');
+const pathsEl = $('#paths');
 
 // #endregion
 
@@ -47,16 +73,16 @@ const resultsEl = $('#results');
 
 const READY = Symbol();
 const DRAWING = Symbol();
+const DRAGGING = Symbol();
 const CALCULATING = Symbol();
 const SVG_NS = 'http://www.w3.org/2000/svg';
-const RECT_SIZE = 8;
-const HALF_RECT_SIZE = RECT_SIZE / 2;
 
 // #endregion
 
 // #region State
 // =============
 
+let image = null;
 let imageName = null;
 let width = 0;
 let height = 0;
@@ -73,24 +99,14 @@ const results = [];
 const lockButtons = () => {
   fileInputEl.disabled = true;
   calculateButtonEl.disabled = true;
-  colorInputEl.disabled = true;
 };
 
 const unlockButtons = () => {
   fileInputEl.disabled = false;
   calculateButtonEl.disabled = false;
-  colorInputEl.disabled = false;
-};
-
-colorInputEl.onchange = () => {
-  const x = colorInputEl.value;
-  svgEl.style.setProperty('--rect', `${x}88`);
-  svgEl.style.setProperty('--fill', `${x}22`);
-  svgEl.style.setProperty('--stroke', `${x}33`);
 };
 
 loadFileEl.onclick = () => fileInputEl.click();
-pathColorEl.onclick = () => colorInputEl.click();
 
 // #endregion
 
@@ -102,27 +118,29 @@ fileInputEl.onchange = () => {
   lockButtons();
   const fr = new FileReader();
   fr.onload = () => {
-    const image = new Image();
-    image.onload = () => setImage(image);
-    image.src = fr.result;
+    const _image = new Image();
+    _image.onload = () => {
+      setImage(_image, fileInputEl.files[0].name);
+      fileInputEl.value = null;
+    };
+    _image.src = fr.result;
   };
   fr.readAsDataURL(fileInputEl.files[0]);
 };
 
-const setImage = image => {
-  imageName = fileInputEl.files[0].name;
-  width = canvasEl.width = image.width;
-  height = canvasEl.height = image.height;
+const setImage = (_image, name) => {
+  image = _image;
+  imageName = name;
+  width = canvasEl.width = _image.width;
+  height = canvasEl.height = _image.height;
   svgEl.setAttribute('viewBox', `0 0 ${width} ${height}`);
-  ctx.drawImage(image, 0, 0, width, height);
+  ctx.drawImage(_image, 0, 0, width, height);
   for (const path of paths) {
-    for (const point of path) {
-      svgEl.removeChild(point.rect);
-    }
+    svgEl.removeChild(path.groupEl);
   }
   paths = [];
   currentPath = null;
-  updatePath();
+  updatePaths();
   unlockButtons();
 };
 
@@ -133,43 +151,49 @@ const setImage = image => {
 
 svgEl.oncontextmenu = evt => evt.preventDefault();
 svgEl.onmousedown = evt => {
-  if (mode === CALCULATING) { return; }
+  if (imageName === null || mode === CALCULATING) { return; }
   if (evt.which === 1) { return handleLeftClick(evt); }
   if (evt.which === 3) { return handleRightClick(evt); }
 };
 
 window.onmousemove = evt => {
   if (mode === DRAWING) {
-    updatePath(evt);
+    updatePaths(evt);
   }
 };
 
 const handleLeftClick = evt => {
-  if (evt.target.tagName === 'rect') { return beginDragPoint(evt.target); }
+  if (evt.target.tagName === 'rect' && mode === READY) { return beginDragPoint(evt.target); }
   if (mode === DRAWING) { return addPoint(evt.offsetX, evt.offsetY); }
   if (mode === READY) { return beginPath(evt.offsetX, evt.offsetY); }
 };
 
 const handleRightClick = () => {
   if (mode === DRAWING) {
-    if (currentPath.length < 3) { return cancelPath();}
+    if (currentPath.points.length < 3) { return cancelPath();}
     return endPath();
   }
 };
 
 const addPoint = (x, y) => {
-  const rect = el('rect', {
-    x: x - HALF_RECT_SIZE,
-    y: y - HALF_RECT_SIZE,
-  });
-  svgEl.appendChild(rect);
-  currentPath.push({x, y, rect});
-  updatePath();
+  const rect = el('rect', {x, y, 'data-path-index': paths.length});
+  currentPath.groupEl.appendChild(rect);
+  currentPath.points.push({x, y, rect});
+  updatePaths();
 };
 
 const beginPath = (x, y) => {
   mode = DRAWING;
-  currentPath = [];
+  currentPath = {name: `Path ${paths.length + 1}`, points: []};
+  const pathEl1 = el('path', {
+    class: 'path1',
+    onmouseenter: highlightPath(currentPath),
+    onmouseleave: unhighlightPath(currentPath),
+  });
+  const pathEl2 = el('path', {class: 'path2'});
+  const groupEl = el('g', {}, [pathEl1, pathEl2]);
+  svgEl.appendChild(groupEl);
+  Object.assign(currentPath, {pathEl1, pathEl2, groupEl});
   paths.push(currentPath);
   lockButtons();
   addPoint(x, y);
@@ -178,34 +202,19 @@ const beginPath = (x, y) => {
 const endPath = () => {
   mode = READY;
   currentPath = null;
-  updatePath();
+  updatePaths();
   unlockButtons();
 };
 
 const cancelPath = () => {
-  currentPath.forEach(point => svgEl.removeChild(point.rect));
+  svgEl.removeChild(currentPath.groupEl);
   paths.pop();
   endPath();
 };
 
-const updatePath = evt => {
-  let d = '';
-  for (const path of paths) {
-    d += `M${path[0].x},${path[0].y}`;
-    for (let i=1; i<path.length; i++) {
-      d += `L${path[i].x},${path[i].y}`;
-    }
-  }
-  if (mode === DRAWING && evt) {
-    d += `L${evt.offsetX},${evt.offsetY}`;
-  }
-  paths.length > 0 && (d += 'Z');
-  pathEl.setAttribute('d', d);
-};
-
 const pointFromRect = rect => {
   for (const path of paths) {
-    for (const point of path) {
+    for (const point of path.points) {
       if (point.rect === rect) {
         return point;
       }
@@ -214,21 +223,128 @@ const pointFromRect = rect => {
 };
 
 const beginDragPoint = rect => {
+  mode = DRAGGING;
   const point = pointFromRect(rect);
   if (!point) { return; }
   const dragMove = evt => {
     point.x = evt.offsetX;
     point.y = evt.offsetY;
-    rect.setAttribute('x', evt.offsetX - HALF_RECT_SIZE);
-    rect.setAttribute('y', evt.offsetY - HALF_RECT_SIZE);
-    updatePath();
+    rect.setAttribute('x', evt.offsetX);
+    rect.setAttribute('y', evt.offsetY);
+    updatePaths();
   };
   const dragEnd = () => {
     window.removeEventListener('mousemove', dragMove);
     window.removeEventListener('mouseup', dragEnd);
+    mode = READY;
+    updatePaths();
   };
   window.addEventListener('mousemove', dragMove);
   window.addEventListener('mouseup', dragEnd);
+};
+
+// #endregion
+
+// #region Paths
+// =============
+
+const renamePath = path => {
+  const newName = prompt('New name:', path.name);
+  if (!newName) { return; }
+  path.name = newName;
+  updatePaths();
+};
+
+const deletePath = path => {
+  paths.splice(paths.indexOf(path), 1);
+  svgEl.removeChild(path.groupEl);
+  updatePaths();
+}
+
+const handlePathMenuSelect = path => evt => {
+  switch (evt.target.value) {
+    case 'rename': {renamePath(path); break;}
+    case 'delete': {deletePath(path); break;}
+  }
+};
+
+const squareBounds = bounds => {
+  const {left, right, top, bottom, width, height} = bounds;
+  const size = Math.max(width, height);
+  const centerX = left + width / 2;
+  const centerY = top + height / 2;
+  return {
+    left: centerX - size / 2,
+    right: centerX + size / 2,
+    top: centerY - size / 2,
+    bottom: centerY + size / 2,
+    width: size,
+    height: size,
+  };
+};
+
+const addPathEl = path => {
+  const menuEl = el('select', {class: 'dropdown', onchange: handlePathMenuSelect(path)}, [
+    el('option', {value: 'rename'}, ['Rename']),
+    el('option', {value: 'delete'}, ['Delete']),
+  ]);
+  const id = Math.random().toString(36).slice(2);
+  menuEl.value = '';
+  const bounds = squareBounds(getPathsBounds([path]));
+
+  const sidebarEl = (
+    el('div', {class: 'path'}, [
+      el('svg', {
+        class: 'path-thumbnail',
+        viewBox: `${bounds.left} ${bounds.top} ${bounds.width} ${bounds.height}`,
+      }, [
+        el('defs', {}, [
+          el('clipPath', {id}, [
+            el('path', {d: path.pathEl1.getAttribute('d')}),
+          ]),
+        ]),
+        el('image', {href: image.src, x: 0, y: 0, width, height, 'clip-path': `url(#${id})`}),
+      ]),
+      el('div', {class: 'path-name', title: path.name}, [path.name]),
+      el('div', {class: 'button dropdown-button'}, ['▼', menuEl]),
+    ])
+  );
+  pathsEl.appendChild(sidebarEl);
+  path.sidebarEl = sidebarEl;
+};
+
+const updatePaths = evt => {
+  if (mode === READY) {
+    pathsEl.innerHTML = '';
+  }
+  for (const path of paths) {
+    let d = '';
+    d += `M${path.points[0].x},${path.points[0].y}`;
+    for (let i=1; i<path.points.length; i++) {
+      d += `L${path.points[i].x},${path.points[i].y}`;
+    }
+    if (mode === DRAWING && path === currentPath && evt) {
+      d += `L${evt.offsetX},${evt.offsetY}`;
+    }
+    d += 'Z';
+    path.pathEl1.setAttribute('d', d);
+    path.pathEl2.setAttribute('d', d);
+    if (mode === READY) {
+      if (path !== currentPath) {
+        addPathEl(path);
+      }
+    }
+  }
+};
+
+const highlightPath = path => () => {
+  if (mode === DRAWING || !path) { return; }
+  path.sidebarEl.classList.add('highlighted');
+};
+
+const unhighlightPath = path => () => {
+  if (mode === DRAWING || !path) { return; }
+  path.sidebarEl.classList.remove('highlighted');
 };
 
 // #endregion
@@ -243,17 +359,12 @@ calculateButtonEl.onclick = () => {
 };
 
 const calculateFromPath = () => {
-  const pathRect = pathEl.getBoundingClientRect();
-  const svgRect = svgEl.getBoundingClientRect();
-  const pathX = pathRect.x - svgRect.x;
-  const pathY = pathRect.y - svgRect.y;
-  const pathWidth = pathRect.width;
-  const pathHeight = pathRect.height;
+  const pathsBounds = getPathsBounds(paths);
   const imageData = ctx.getImageData(0, 0, width, height);
-  const pathD = pathEl.getAttribute('d');
-  const landscape = pathWidth > pathHeight;
+  const pathsD = paths.map(path => path.pathEl1.getAttribute('d')).join('');
+  const landscape = pathsBounds.width > pathsBounds.height;
   const threads = navigator.hardwareConcurrency ?? 4;
-  const sliceSize = landscape ? Math.ceil(pathWidth / threads) : Math.ceil(pathHeight / threads);
+  const sliceSize = landscape ? Math.ceil(pathsBounds.width / threads) : Math.ceil(pathsBounds.height / threads);
 
   let workersPending = 0;
   let red = 0;
@@ -275,30 +386,30 @@ const calculateFromPath = () => {
   };
 
   if (landscape) {
-    for (let x = pathX; x < pathX + pathWidth; x += sliceSize) {
+    for (let x = pathsBounds.left; x < pathsBounds.right; x += sliceSize) {
       const worker = new Worker('./worker.js');
       worker.onmessage = onMessage;
       worker.postMessage({
         imageData,
-        pathD,
+        pathsD,
         minX: x,
-        maxX: Math.min(pathX + pathWidth, x + sliceSize),
-        minY: pathY,
-        maxY: pathY + pathHeight,
+        maxX: Math.min(pathsBounds.right, x + sliceSize),
+        minY: pathsBounds.top,
+        maxY: pathsBounds.bottom,
       });
       workersPending++;
     }
   } else {
-    for (let y = pathY; y < pathY + pathHeight; y += sliceSize) {
+    for (let y = pathsBounds.top; y < pathsBounds.bottom; y += sliceSize) {
       const worker = new Worker('./worker.js');
       worker.onmessage = onMessage;
       worker.postMessage({
         imageData,
-        pathD,
-        minX: pathX,
-        maxX: pathX + pathWidth,
+        pathsD,
+        minX: pathsBounds.left,
+        maxX: pathsBounds.right,
         minY: y,
-        maxY: Math.min(pathY + pathHeight, y + sliceSize),
+        maxY: Math.min(pathsBounds.right, y + sliceSize),
       });
       workersPending++;
     }
@@ -345,25 +456,23 @@ const renameResult = result => {
   updateResults();
 };
 
-const handleMenuSelect = result => evt => {
+const handleResultMenuSelect = result => evt => {
   switch (evt.target.value) {
-    case 'copy-hex': return copyHex(result);
-    case 'copy-css-rgb': return copyCssRgb(result);
-    case 'copy-css-rgb': return copyCssRgb(result);
-    case 'copy-blender': return copyBlender(result);
-    case 'rename': return renameResult(result);
-    case 'delete': return deleteResult(result);
+    case 'copy-hex': {copyHex(result); break;}
+    case 'copy-css-rgb': {copyCssRgb(result); break;}
+    case 'copy-css-rgb': {copyCssRgb(result); break;}
+    case 'copy-blender': {copyBlender(result); break;}
+    case 'rename': {renameResult(result); break;}
+    case 'delete': {deleteResult(result); break;}
   }
   evt.target.value = '';
   evt.target.blur();
 };
 
-let averageResult = null;
-
 const addResultEl = (result, allowDelete = true) => {
   const {r, g, b, name} = result;
   const dark = (r + g + b) / 3 < 128;
-  const menuEl = el('select', {class: 'result-menu', value: '', onchange: handleMenuSelect(result)}, [
+  const menuEl = el('select', {class: 'dropdown', onchange: handleResultMenuSelect(result)}, [
     el('option', {value: 'copy-hex'}, ['Copy as hex']),
     el('option', {value: 'copy-css-rgb'}, ['Copy as CSS rgb()']),
     el('option', {value: 'copy-blender'}, ['Copy as Blender color']),
@@ -381,7 +490,7 @@ const addResultEl = (result, allowDelete = true) => {
       },
     }, [
       el('div', {class: 'result-name', title: name}, [name]),
-      el('div', {class: 'button result-menu-button'}, [
+      el('div', {class: 'button dropdown-button'}, [
         '▼',
         menuEl,
       ]),
@@ -399,8 +508,7 @@ const updateResults = () => {
       ([ra, ga, ba], {r, g, b}) => [ra + r ** 2, ga + g ** 2, ba + b ** 2],
       [0, 0, 0]
     ).map(x => Math.sqrt(x / results.length));
-    averageResult = {r, g, b, name: 'Average of all results'};
-    addResultEl(averageResult, false);
+    addResultEl({r, g, b, name: 'Average of all results'}, false);
   }
 };
 
--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 onmessage = e => {
-  const {imageData, pathD, minX, maxX, minY, maxY} = e.data;
+  const {imageData, pathsD, minX, maxX, minY, maxY} = e.data;
   const canvas = new OffscreenCanvas(imageData.width, imageData.height);
   const ctx = canvas.getContext('2d', {willReadFrequently: true});
   ctx.putImageData(imageData, 0, 0);
-  const path = new Path2D(pathD);
+  const path = new Path2D(pathsD);
   const result = [];
   for (let x=minX; x<maxX; x++) {
     for (let y=minY; y<maxY; y++) {
